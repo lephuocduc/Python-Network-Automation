b615689225b1e4379e2dc084fa171d7668ffe1fd
Duc Le <leducst1@gmail.com>
2024-02-19 15:24:07 +0700

    Modified CheckIPInterface-FromFile.py for prompting username and password

--- Day6/CheckIPInterface-FromFile.py
+++ Day6/CheckIPInterface-FromFile.py
@@ -1,13 +1,18 @@
 # Check IP interface: Create a list of interface randomly. Then apply the Python script to check the information automatically.
 
 from netmiko import ConnectHandler
+import getpass
+
+# Prompt for username and password
+username = input("Enter your username: ")
+password = getpass.getpass()
 
 connection_info = {
     'device_type': 'huawei',
     'host': '10.224.130.1',
     'port': 22,
-    'username': 'admin',
-    'password': '12345678a@'
+    'username': username,
+    'password': password
 }
 
 # Open VLANList.txt and read its contents
