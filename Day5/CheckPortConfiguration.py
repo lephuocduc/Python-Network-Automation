8d05e1aa453c31df8cf98afa6bee17e1ee03c3c3
Duc Le <leducst1@gmail.com>
2024-02-19 10:59:25 +0700

    Added CheckPortConfiguration.py

---
+++ Day5/CheckPortConfiguration.py
@@ -0,0 +1,13 @@
+from neimiko import ConnectHandler
+
+connection_info = {
+    'device_type': 'huawei',
+    'host': '10.224.130.1',
+    'port': 22,
+    'username': 'admin',
+    'password': '12345678a@'
+}
+
+with ConnectHandler(**connection_info) as conn:
+		out = conn.send_command("display interface brief")
+		print(out)
