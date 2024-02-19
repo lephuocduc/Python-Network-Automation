1c632818853fbe74ca971addcf8645305f6826d5
Duc Le <leducst1@gmail.com>
2024-02-19 16:13:24 +0700

    commit

--- Day5/AddPortDescription.py
+++ Day5/AddPortDescription.py
@@ -10,11 +10,13 @@ connection_info = {
 
 with ConnectHandler(**connection_info) as conn:
 		conn.enable()
+		
 config_commands = [
-        "interface 10GE 2/0/40", 
-        "description HelloABC",
-				"commit"
-    ]
+	"interface 10GE 2/0/40", 
+	"description HelloABC",
+	"commit"
+	]
+
 output = conn.send_config_set(config_commands)
 
 print(output)
