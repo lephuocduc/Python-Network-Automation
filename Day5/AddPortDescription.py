8c8d062290d0d328adf11cda36c15f033c21140f
Duc Le <leducst1@gmail.com>
2024-02-19 11:12:50 +0700

    Added AddPortDescription.py

---
+++ Day5/AddPortDescription.py
@@ -0,0 +1,20 @@
+from netmiko import ConnectHandler
+
+connection_info = {
+    'device_type': 'huawei',
+    'host': '10.224.130.1',
+    'port': 22,
+    'username': 'admin',
+    'password': '12345678a@'
+}
+
+with ConnectHandler(**connection_info) as conn:
+		conn.enable()
+config_commands = [
+        "interface 10GE 2/0/40", 
+        "description HelloABC",
+				"commit"
+    ]
+output = conn.send_config_set(config_commands)
+
+print(output)
