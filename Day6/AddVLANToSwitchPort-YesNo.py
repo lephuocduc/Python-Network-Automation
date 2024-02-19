bb64584ee32123eb3bc01916c4d15070c2297210
Duc Le <leducst1@gmail.com>
2024-02-19 18:28:11 +0700

    commit

---
+++ Day6/AddVLANToSwitchPort-YesNo.py
@@ -0,0 +1,6 @@
+"""
+    *Assign VLAN to switch port:*
+    
+    Pre-check all vlan infor & vlan interface.
+    Yes/No option for execution.
+"""
---
+++ Day6/RemovePortDescription-YesNo.py
@@ -0,0 +1,4 @@
+"""
+    Pre-check current port description.(Show all port description)
+    Yes/No option for execution.
+"""
