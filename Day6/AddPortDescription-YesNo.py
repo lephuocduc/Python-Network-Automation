28f9322ded90b441951db08b9ec81663fe861ddf
DucLe <leducst1@gmail.com>
2024-02-21 06:49:46 +0000

    commit

--- Day6/AddPortDescription-YesNo.py
+++ Day6/AddPortDescription-YesNo.py
@@ -3,7 +3,7 @@
     Yes/No option for execution.
 """
 
-from netmiko import ConnectHandler
+from netmiko import ConnectHandler, NetMikoAuthenticationException
 import getpass
 
 # Prompting for credentials
@@ -36,33 +36,38 @@ def change_description(conn, port, newdescription):
 
 def main():
       #Main function
-      username, password = get_credentials()
+      while True:
+            username, password = get_credentials()
 
-      connection_info = {
-        'device_type': 'huawei',
-        'host': '10.224.130.1',
-        'port': 22,
-        'username': username,
-        'password': password
-        }
-      
-      port, newdescription = get_port_and_newdescription()
+            connection_info = {
+            'device_type': 'huawei',
+            'host': '10.224.130.1',
+            'port': 22,
+            'username': username,
+            'password': password
+            }            
 
-      try:
-            with ConnectHandler(**connection_info) as conn:
-                  description = get_current_description(conn, port)
-                  while True:
-                        confirmation = input(f"Do you want to change port description of '{port}' from '{description}' to '{newdescription}'? Yes/ No: ")
-                        if confirmation.lower() == "yes":
-                            change_description(conn, port, newdescription)
-                            break
-                        if confirmation.lower() == "no":
-                            print("Operation cancelled!")
-                            break
-                        else:
-                            continue
-      except Exception as e:
-            print(f"An error has occured: {e}")
+            try:
+                  with ConnectHandler(**connection_info) as conn:
+                        port, newdescription = get_port_and_newdescription()
+                        description = get_current_description(conn, port)
+                        while True:
+                              confirmation = input(f"Do you want to change port description of '{port}' from '{description}' to '{newdescription}'? Yes/ No: ")
+                              if confirmation.lower() == "yes":
+                                    change_description(conn, port, newdescription)
+                                    break
+                              if confirmation.lower() == "no":
+                                    print("Operation cancelled!")
+                                    break
+                              else:
+                                    continue
+                        break
+            except NetMikoAuthenticationException:
+                  print("Invalid credentials, please try again")
+                  continue
+            except Exception as e:
+                  print(f"An error has occured: {e}")
+                  break
 
 if __name__ == "__main__":
     main()
