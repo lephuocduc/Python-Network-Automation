93e702112dd1a8950d85cc7e127b3a226ea33cba
DucLe <leducst1@gmail.com>
2024-02-27 02:36:55 +0000

    commit

---
+++ Day7/AddVLANToSwitchPort-YesNo.py
@@ -0,0 +1,83 @@
+"""
+    *Assign VLAN to switch port:*
+
+    Pre-check all vlan infor & vlan interface.
+    Yes/No option for execution.
+    1. Choose Yes -> Export backup configuration file for port -> execute the script changing configuration. (Many ports to be assigned to one Vlan / Many ports to be assigned to many appropriate VLAN)
+    2. Choose No -> Stop the script.  
+"""
+
+"""
+from netmiko import ConnectHandler, NetMikoAuthenticationException
+import getpass
+
+def get_credentials():
+    username = input("Enter your username: ")
+    password = getpass.getpass()
+    return username, password
+
+def get_port_and_vlan():
+    port = input("Enter your port: ")
+    vlan = input("Enter your Vlan to assign: ")
+    return port, vlan
+
+def get_current_vlan_info(conn, vlan):
+    out = conn.send_command(f"display {vlan}")
+    print(f"BELOW IS YOUR {vlan}")
+    print(out)
+
+def get_current_switchport_info(conn, port):
+    out = conn.send_command(f"display interface {port}")
+    print(f"BELOW IS YOUR PORT {port}")
+    print(out)
+
+def add_vlan_to_switchport(conn, port, vlan):
+    conn.enable()
+    config_commands = [
+        f'interface {port}',
+        'port link-type access',
+        f'port default {vlan}',
+		'commit'
+    ]
+    output = conn.send_config_set(config_commands)
+    print(output)
+
+def main():
+    while True:
+        username, password = get_credentials()
+
+        connection_info = {
+            'device_type': 'huawei',
+            'host': '10.224.130.1',
+            'port': 22,
+            'username': username,
+            'password': password
+            }
+        
+        try:
+            with ConnectHandler(**connection_info) as conn:
+                print("Connected successfully")
+                port, vlan = get_port_and_vlan()
+                get_current_vlan_info(conn, vlan)
+                get_current_switchport_info(conn, port)
+                while True:
+                    confirmation = input(f"Do you want to assign {vlan} to {port}? Yes/ No: ")
+                    if confirmation.lower() == "yes":
+                        add_vlan_to_switchport(conn, port, vlan)
+                        break
+                    if confirmation.lower() == "no":
+                        print("Operation cancelled!")
+                        break
+                    else:
+                        continue
+                break
+        except NetMikoAuthenticationException:
+            print("Invalid credentials. Please try again.")
+            continue
+        except Exception as e:
+            print(f"An error has occured: {e}")
+            break
+
+if __name__ == "__main__":
+    main()
+    """
---
+++ Day7/RemovePortDescription-YesNo.py
@@ -0,0 +1,75 @@
+"""
+    Pre-check current port description.(Show all port description)
+    Yes/No option for execution.
+    1. Choose Yes -> Export backup configuration file for port è execute the script changing configuration (Remove Many port description at the same time)
+    2. Choose No -> Stop the script.  
+"""
+
+"""
+from netmiko import ConnectHandler, NetMikoAuthenticationException
+import getpass
+
+def get_credentials():
+    username = input("Enter your username: ")
+    password = getpass.getpass()
+    return username, password
+
+def get_port():
+    port = input("Enter your port to remove description: ")
+    return port
+
+def get_current_description(conn, port):
+    out = conn.send_command(f"display interface {port}")
+    lines = out.split('\n')
+    for line in lines:
+        if "Description:" in line:
+            return line.split ("Description:")[1].strip()
+
+def remove_port_description(conn, port):
+    conn.enable()
+    config_commands = [
+          f"interface {port}",
+          "undo description",
+          "commit"
+    ]
+    out = conn.send_config_set(config_commands)
+    print(out)
+
+
+def main():
+    while True:
+        username, password = get_credentials()
+        
+        connection_info = {
+            'device_type': 'huawei',
+            'host': '10.224.130.1',
+            'port': 22,
+            'username': username,
+            'password': password
+        }
+
+        try:
+            with ConnectHandler(**connection_info) as conn:
+                print("Connected successfully")
+                port = get_port()
+                description = get_current_description(conn, port)
+                while True:
+                    confirmation = input(f"Do you want to remove port description of '{port}' from '{description}'? Yes/ No: ")
+                    if confirmation.lower() == 'yes':
+                        remove_port_description(conn, port)
+                        break
+                    if confirmation.lower() == 'no':
+                        print("Operation cancelled!")
+                        break
+                    else: 
+                        continue
+                break
+        except NetMikoAuthenticationException:
+            print("Invalid credentials, please try again!")
+            continue
+        except Exception as e:
+            print(f"An error has occured: {e}")
+            break
+if __name__ == "__main__":
+    main()
+"""
