4da8c35f0107b10c6e5f587bf16c5632a673cd2d
DucLe <leducst1@gmail.com>
2024-03-23 03:34:54 +0000

    commit AddPortDescription

--- Day9/AddPortDescription.py
+++ Day9/AddPortDescription.py
@@ -11,6 +11,7 @@ User to be asked for the path of the Port list or type All to execute the script
 
 *Need Standardizing
 """
+#Scrip done, need standardizing
 
 from netmiko import ConnectHandler, NetMikoAuthenticationException
 from datetime import datetime
@@ -22,15 +23,22 @@ def get_credentials():
       password = getpass.getpass()
       return username, password
 
-def get_ports_and_newdescription(file_path):
-      ports =[]
-      newdescriptions = []
-      with open(f'{file_path}','r') as file:
-            for line in file:
-                  port, newdescription = line.strip().split(" | ")
-                  ports.append(port)
-                  newdescriptions.append(newdescription)
-      return ports, newdescriptions
+def get_ports_and_new_descriptions_from_file():
+    ports =[]
+    new_descriptions = []
+    while True:
+        try:
+            file_path = input("Enter your file path: ")
+            with open(f'{file_path}','r') as file:
+                for line in file:
+                    port, new_description = line.strip().split(" | ")
+                    ports.append(port)
+                    new_descriptions.append(new_description)
+            return ports, new_descriptions
+
+        except FileNotFoundError:
+            print("File not found. Please try again!")
+            continue #If the file is not found, countinue the loop
 
 def get_current_description(conn, port):
       out = conn.send_command(f"display interface {port}")
@@ -46,11 +54,11 @@ def backup_config(conn):
         f.write(backup)
         print("Backup configuration has been saved to backup_config.txt")
 
-def change_description(conn, port, newdescription):
+def change_description(conn, port, new_description):
       conn.enable()
       config_commands = [
             f"interface {port}",
-            f"description {newdescription}",
+            f"description {new_description}",
             "commit"
             ]
       out = conn.send_config_set(config_commands)
@@ -65,105 +73,95 @@ def check_port_exists(conn, port):
 
 def main():
     #Main function
-    try:
-        while True:
-            username, password = get_credentials()
+    while True:
+        username, password = get_credentials()
 
-            connection_info = {
-            'device_type': 'huawei',
-            'host': '10.224.130.1',
-            'port': 22,
-            'username': username,
-            'password': password
-            }            
+        connection_info = {
+        'device_type': 'huawei',
+        'host': '10.224.130.1',
+        'port': 22,
+        'username': username,
+        'password': password
+        }            
 
-            try:
-                with ConnectHandler(**connection_info) as conn:
-                    print("Connected Successfully")
-                    break
-
-            except NetMikoAuthenticationException:
-                print("Invalid credentials, please try again")
-                continue
-
-        while True:
-            confirmation = input("Type '1' to enter the path. Type '2' to apply a new description for all Ports: ")
-            if confirmation == "1":
+        try:
+            with ConnectHandler(**connection_info) as conn:
+                print("Connected Successfully")  
                 while True:
-                    try:
-                        file_path = input("Enter your file path: ")
-                        ports, newdescriptions = get_ports_and_newdescription(file_path)
-                        break #If the file is found and read successfully, break the loop
-                    except FileNotFoundError:
-                        print("File not found. Please try again!")
-                        continue #If the file is not found, countinue the loop
-
-                for port, newdescription in zip(ports, newdescriptions):
-                    print(f"Change description of port '{port}' from '{get_current_description(conn,port)}' to '{newdescription}'")
-                while True:
-                    confirmation = input("Do you want to execute the scipt? Yes/ No: ")
-                    if confirmation.lower() == "yes":
-                        with ConnectHandler(**connection_info) as conn:
-                            backup_config(conn)
-                            for port, newdescription in zip(ports, newdescriptions):
-                                change_description(conn, port, newdescription)
-                            print("All descriptions after changed")
-                            for port in ports:
-                                print(f"The current description of port '{port}' is: {get_current_description(conn, port)}")
-                        break
-
-                    if confirmation.lower() == "no":
-                        print("Operation cancelled!")
-                        break
-
-                    else:
-                            continue
-                break
-
-            if confirmation == "2":
-                ports = []
-                number = 1
-                with ConnectHandler(**connection_info) as conn:
-                    while True:
-                        port = input(f"Enter your port {number}: ")
-                        if port == "":
-                            break
-                        else:  
-                            if check_port_exists(conn, port):
-                                ports.append(port)
-                                number += 1
-                                continue
-
-                            else:
-                                print(f"Port '{port}' does not exist. Please try again.")
-                                continue
-                    newdescription = input("Enter new description: ")
-                    for port in ports:
-                        print(f"The current description of port '{port}' is: {get_current_description(conn, port)}")
-                    while True:
-                            confirmation = input(f"Do you want to change all descriptions to '{newdescription}'? Yes/ No: ")
+                    confirmation = input("Type '1' to enter the path. Type '2' to apply a new description for all Ports: ")
+                    if confirmation == "1":
+                        ports, new_descriptions = get_ports_and_new_descriptions_from_file()                          
+                        for port, new_description in zip(ports, new_descriptions):
+                            print(f"Change description of port '{port}' from '{get_current_description(conn,port)}' to '{new_description}'")
+                        while True:
+                            confirmation = input("Do you want to execute the scipt? Yes/ No: ")
                             if confirmation.lower() == "yes":
                                 backup_config(conn)
-                                for port in ports:
-                                        change_description(conn, port, newdescription)
+                                for port, new_description in zip(ports, new_descriptions):
+                                    change_description(conn, port, new_description)
                                 print("All descriptions after changed")
                                 for port in ports:
-                                        print(f"The current description of port '{port}' is: {get_current_description(conn, port)}")
+                                    print(f"The current description of port '{port}' is: {get_current_description(conn, port)}")
                                 break
-                            
+
                             if confirmation.lower() == "no":
                                 print("Operation cancelled!")
                                 break
-                            
+
                             else:
-                                continue
-                break
+                                    continue
+                        break
 
-            else:
-                continue                         
+                    if confirmation == "2":
+                        ports = []
+                        number = 1
+                        while True:
+                            port = input(f"Enter your port {number}: ")
+                            if port == "":
+                                break
 
-    except Exception as e:
-        print(f"An error has occured: {e}")                 
+                            else:  
+                                if check_port_exists(conn, port):
+                                    ports.append(port)
+                                    number += 1
+                                    continue
+
+                                else:
+                                    print(f"Port '{port}' does not exist. Please try again.")
+                                    continue
+
+                        new_description = input("Enter new description: ")
+                        for port in ports:
+                            print(f"The current description of port '{port}' is: {get_current_description(conn, port)}")
+                        while True:
+                                confirmation = input(f"Do you want to change all descriptions to '{new_description}'? Yes/ No: ")
+                                if confirmation.lower() == "yes":
+                                    backup_config(conn)
+                                    for port in ports:
+                                            change_description(conn, port, new_description)
+                                    print("All descriptions after changed")
+                                    for port in ports:
+                                            print(f"The current description of port '{port}' is: {get_current_description(conn, port)}")
+                                    break
+                                
+                                if confirmation.lower() == "no":
+                                    print("Operation cancelled!")
+                                    break
+                                
+                                else:
+                                    continue
+                        break
+
+                    else:
+                        continue
+            break
+
+        except NetMikoAuthenticationException:
+            print("Invalid credentials, please try again")
+            continue
+
+        except Exception as e:
+            print(f"An error has occured: {e}")                 
 
 if __name__ == "__main__":
     main()
